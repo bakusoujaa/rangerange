3266627dd03c33baa96cfe4110cd18172104acf6
k-shirahata <k-shirahata@ruby-dev.jp>
2021-04-03 16:55:13 +0900

    define isCover method

--- src/TypeConfigs/Integer.test.ts
+++ src/TypeConfigs/Integer.test.ts
@@ -1,6 +1,6 @@
 import Rangerange from '../'
 
-describe('number', () => {
+describe('Integer', () => {
   let minimum = null
   let maximum = null
 
@@ -36,6 +36,278 @@ describe('number', () => {
       it('be blank array', () => {
         expect(rangerange.toArray()).toStrictEqual([])
       })
+    })
+  })
+
+  describe('isCover()', () => {
+    let self: Rangerange<number>
+    let selfMin: number | undefined
+    let selfMax: number | undefined
+
+    describe('method attr is Rangerange object', () => {
+      let other: Rangerange<number>
+      let otherMin: number | undefined
+      let otherMax: number | undefined
+
+      beforeEach(() => {
+        otherMin = 10
+        otherMax = 100
+      })
+
+      describe('self has min and max', () => {
+        beforeEach(() => {
+          selfMin = 10
+          selfMax = 100
+        })
+
+        describe('other has min and max', () => {
+          describe('self.min <= other.min && other.max <= self.max', () => {
+            beforeEach(() => {
+              otherMin = 10
+              otherMax = 100
+              self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+              other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+            })
+            it('should be true', () => {
+              expect(self.isCover(other)).toBe(true)
+            })
+          })
+
+          describe('other.min <= self.min && other.max <= self.max', () => {
+            beforeEach(() => {
+              otherMin = 9
+              otherMax = 100
+              self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+              other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+            })
+            it('should be false', () => {
+              expect(self.isCover(other)).toBe(false)
+            })
+          })
+
+          describe('self.min <= other.min && self.max <= other.max', () => {
+            beforeEach(() => {
+              otherMin = 10
+              otherMax = 101
+              self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+              other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+            })
+            it('should be false', () => {
+              expect(self.isCover(other)).toBe(false)
+            })
+          })
+        })
+
+        describe('other has not min', () => {
+          beforeEach(() => {
+            otherMin = undefined
+            otherMax = 100
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+
+        describe('other has not max', () => {
+          beforeEach(() => {
+            otherMin = 10
+            otherMax = undefined
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+      })
+
+      describe('self has only min', () => {
+        beforeEach(() => {
+          selfMin = 10
+          selfMax = undefined
+        })
+
+        describe('other has not min', () => {
+          beforeEach(() => {
+            otherMin = undefined
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+
+        describe('self > other', () => {
+          beforeEach(() => {
+            selfMin = 10
+            otherMin = 9
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+
+        describe('self == other', () => {
+          beforeEach(() => {
+            selfMin = 10
+            otherMin = 10
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('self < other', () => {
+          beforeEach(() => {
+            selfMin = 10
+            otherMin = 11
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+      })
+
+      describe('self has only max', () => {
+        beforeEach(() => {
+          selfMin = undefined
+          selfMax = 100
+        })
+
+        describe('other has not max', () => {
+          beforeEach(() => {
+            otherMax = undefined
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+
+        describe('self > other', () => {
+          beforeEach(() => {
+            selfMax = 10
+            otherMax = 9
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('self == other', () => {
+          beforeEach(() => {
+            selfMax = 10
+            otherMax = 10
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('self < other', () => {
+          beforeEach(() => {
+            selfMax = 10
+            otherMax = 11
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            other = new Rangerange<number>({ minimum: otherMin, maximum: otherMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+      })
+    })
+
+    describe('method attr is T object', () => {
+      let other: number
+      describe('self has min and max', () => {
+        beforeEach(() => {
+          selfMin = 10
+          selfMax = 100
+        })
+
+        describe('other < min', () => {
+          beforeEach(() => {
+            other = 9
+            selfMin = 10
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+
+        describe('other == min', () => {
+          beforeEach(() => {
+            other = 10
+            selfMin = 10
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('other > min', () => {
+          beforeEach(() => {
+            other = 11
+            selfMin = 10
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+            console.dir(self)
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('other < max', () => {
+          beforeEach(() => {
+            other = 99
+            selfMax = 100
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('other == max', () => {
+          beforeEach(() => {
+            other = 100
+            selfMax = 100
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+          })
+          it('should be true', () => {
+            expect(self.isCover(other)).toBe(true)
+          })
+        })
+
+        describe('other > max', () => {
+          beforeEach(() => {
+            other = 101
+            selfMax = 100
+            self = new Rangerange<number>({ minimum: selfMin, maximum: selfMax })
+          })
+          it('should be false', () => {
+            expect(self.isCover(other)).toBe(false)
+          })
+        })
+      })
+
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,118 @@ import TypeConfigs from './TypeConfigs'
 import ITypeConfig from './TypeConfigs/IRegisterTypeConfig'
 
 export default class Rangerange<T> {
-  private readonly minimum?: T
-  private readonly maximum?: T
+  public readonly minimum?: T
+  public readonly maximum?: T
   private readonly typeConfig: ITypeConfig<T>
 
   constructor(params: { minimum?: T, maximum?: T }) {
-    if (params.minimum && params.maximum) {
-      const minConf = this.getTypeConfig(params.minimum)
-      const maxConf = this.getTypeConfig(params.maximum)
-      if (minConf !== maxConf) { throw new Error('minimum type and maximum type are not much.') }
-      this.typeConfig = minConf
-    } else if (params.minimum) {
-      const minConf = this.getTypeConfig(params.minimum)
-      this.typeConfig = minConf
-    } else if (params.maximum) {
-      const maxConf = this.getTypeConfig(params.maximum)
-      this.typeConfig = maxConf
-    } else {
-      throw new Error('from or to must be present.')
-    }
+    this.typeConfig = this.getTypeConfigByMinOrMax(params)
 
     this.minimum = params.minimum
     this.maximum = params.maximum
   }
 
   toArray = () => {
-    if (!this.minimum || !this.maximum) {
+    if (!this.isPresent(this.minimum) || !this.isPresent(this.maximum)) {
       throw new Error('minimum and maximum must be present.')
     }
     if (!this.typeConfig.stepBy) { throw new Error('typeConfig.succFunc must be exest.') }
 
     let ary = []
-    for (let i = this.minimum; this.typeConfig.compareFunc(i, this.maximum) <= 0; i = this.typeConfig.stepBy(i, 1)) {
+    for (let i = this.minimum as T; this.typeConfig.compareFunc(i, this.maximum as T) <= 0; i = this.typeConfig.stepBy(i, 1)) {
       ary.push(i)
     }
     return ary
   }
 
+  isCover = (other: T | Rangerange<T>) => {
+    if (!this.isPresent(other)) { throw new Error('other must be present.') }
+
+    const _other = (other instanceof Rangerange) ? other :
+      new Rangerange({ minimum: other, maximum: other })
+
+    if (!this.isTypeEquals(_other)) { throw new Error('other type is missmatch.') }
+
+    if (this.isPresent(this.minimum) && this.isPresent(this.maximum)) {
+      const coverMin = this.isCoverMin(_other)
+      const coverMax = this.isCoverMax(_other)
+      return coverMin && coverMax
+    } else if (this.isPresent(this.minimum)) {
+      const coverMin = this.isCoverMin(_other)
+      return coverMin
+    } else if (this.isPresent(this.maximum)) {
+      const coverMax = this.isCoverMax(_other)
+      return coverMax
+    } else {
+      throw new Error('other value is invalid.')
+    }
+  }
+
+  private isCoverMin = (other: Rangerange<T>) => {
+    if (!this.isPresent(this.minimum)) { throw new Error('this method can call only minimum exist.') }
+
+    if (!this.isPresent(other.minimum)) { return false }
+
+    const compareResult = this.typeConfig.compareFunc(this.minimum as T, other.minimum as T)
+
+    if (compareResult === 1) {
+      // this.minimum > _other.minimum
+      return false
+    } else {
+      // this.minimum <= _other.minimum
+      return true
+    }
+  }
+
+  private isCoverMax = (other: Rangerange<T>) => {
+    if (!this.isPresent(this.maximum)) { throw new Error('this method can call only maximum exist.') }
+
+    if (!this.isPresent(other.maximum)) { return false }
+
+    const compareResult = this.typeConfig.compareFunc(this.maximum as T, other.maximum as T)
+
+    if (compareResult === -1) {
+      // this.maximum < _other.maximum
+      return false
+    } else {
+      // this.maximum >= _other.maximum
+      return true
+    }
+  }
+
   private getTypeConfig = (value?: T) => {
     return TypeConfigs.get(value)
+  }
+
+  private getTypeConfigByMinOrMax = (params: { minimum?: T, maximum?: T }) => {
+    if (this.isPresent(params.minimum) && this.isPresent(params.maximum)) {
+      const minConf = this.getTypeConfig(params.minimum)
+      const maxConf = this.getTypeConfig(params.maximum)
+      if (minConf !== maxConf) { throw new Error('minimum type and maximum type are not much.') }
+      return minConf
+    } else if (params.minimum) {
+      const minConf = this.getTypeConfig(params.minimum)
+      return minConf
+    } else if (params.maximum) {
+      const maxConf = this.getTypeConfig(params.maximum)
+      return maxConf
+    } else {
+      throw new Error('from or to must be present.')
+    }
+  }
+
+  private isPresent = (value?: unknown) => {
+    if (value === null) { return false }
+    if (value === undefined) { return false }
+    return true
+  }
+
+  private isTypeEquals = (other: Rangerange<T>) => {
+    const otherTypeConfig = this.getTypeConfigByMinOrMax({
+      minimum: other.minimum,
+      maximum: other.maximum,
+    })
+
+    return this.typeConfig === otherTypeConfig
   }
 }
